d0028aae5738f082abc2ceee0afac94d716ea5e0
filiphalonen <157317694+filiphalonen@users.noreply.github.com>
2024-12-20 20:13:22 +0100

    added breaking and stationary platforms

--- platforms.js
+++ platforms.js
@@ -1,24 +1,33 @@
 class Platform {
-  constructor(x, y, width = 100, height = 10, speed = 2) {
+  constructor(x, y, type = "stationary", width = 100, height = 10, speed = 2) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.speed = speed;
     this.direction = 1;
+    this.type = type;
   }
 
-  // platform moving right and switches to the left when hitting the border, vice-versa
+  // moving platforms switch direction when hitting the border
   move() {
-    this.x += this.speed * this.direction;
-    if (this.x <= 0 || this.x + this.width >= width) {
-      this.direction *= -1;
+    if (this.type === "moving") {
+      this.x += this.speed * this.direction;
+      if (this.x <= 0 || this.x + this.width >= width) {
+        this.direction *= -1;
+      }
     }
   }
 
-  // draw platform as green ish rectangle
+  // draw the platforms based on type
   draw() {
-    fill(0, 200, 120);
+    if (this.type === "breaking") {
+      fill(200, 0, 0);
+    } else if (this.type === "moving") {
+      fill(0, 200, 120);
+    } else {
+      fill(0, 100, 255);
+    }
     rect(this.x, this.y, this.width, this.height);
   }
 }
--- sketch.js
+++ sketch.js
@@ -6,11 +6,12 @@ function setup() {
   createCanvas(550, 700);
   player = new Character();
 
-  // loop for platforms
+  // loop for random platforms
   for (let i = 0; i < 5; i++) {
     let x = random(width - 100);
     let y = height - i * 150;
-    platforms.push(new Platform(x, y));
+    let type = random(["stationary", "moving", "breaking"]);
+    platforms.push(new Platform(x, y, type));
   }
 }
 
@@ -21,24 +22,40 @@ function draw() {
   fill(0, 100, 225);
   rect(0, height - 20, width, 20);
 
-  // movement and game mechanics
+  // move and draw platforms
   for (let platform of platforms) {
     platform.move();
     platform.draw();
+  }
 
-    // platform detection
+  // check for platform collision
+  checkPlatforms();
+
+  // player movement
+  player.move();
+  player.applyGravity();
+  player.draw();
+}
+
+// platform mechanics
+function checkPlatforms() {
+  for (let i = 0; i < platforms.length; i++) {
+    let platform = platforms[i];
+
+    // conditions to land on platforms
     if (
       player.y + player.height >= platform.y &&
-      player.y <= platform.y &&
+      player.y <= platform.y + platform.height &&
       player.x + player.width > platform.x &&
       player.x < platform.x + platform.width &&
       player.velocityY > 0
     ) {
       player.velocityY = player.jumpStrength;
+
+      // remove breaking platforms
+      if (platform.type === "breaking") {
+        platforms.splice(i, 1);
+      }
     }
   }
-
-  player.move();
-  player.applyGravity();
-  player.draw();
 }
