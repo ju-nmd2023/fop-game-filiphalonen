0a7757750e3e6d59b649f8a0694d0e8ea43c8b4c
filiphalonen <157317694+filiphalonen@users.noreply.github.com>
2024-12-25 21:46:23 +0100

    added gamestates + deleted draft
    added start screen, gameplay screen and result screen

--- sketch.js
+++ sketch.js
@@ -1,70 +1,100 @@
 let player;
 let platforms = [];
 let platformSpacing = 150;
+let gameState = "start";
 
 // game environment
 function setup() {
   createCanvas(550, 700);
-  player = new Character();
-
-  // loop for random platforms
-  for (let i = 0; i < 5; i++) {
-    let x = random(width - 100);
-    let y = height - 150 - i * 150;
-    let type = random(["stationary", "moving", "breaking"]);
-    platforms.push(new Platform(x, y, type));
-  }
 }
 
 function draw() {
-  background(0);
-
-  // blue ground
-  fill(0, 100, 225);
-  rect(0, height - 20, width, 20);
-
-  // move and draw platforms
-  for (let platform of platforms) {
-    platform.move();
-    platform.draw();
+  // start screen
+  if (gameState === "start") {
+    background(0);
+    fill(0, 200, 0);
+    textAlign(CENTER, CENTER);
+    textSize(30);
+    text("Welcome to Arcade Doodle Jump!", width / 2, height / 2 - 120);
+    fill(255);
+    textSize(20);
+    text(
+      "Use the arrow keys to move and jump on the platforms!",
+      width / 2,
+      height / 2 - 60
+    );
+    text("Press ENTER to start", width / 2, height / 2 + 20);
   }
+  // gameplay screen
+  else if (gameState === "playing") {
+    background(0);
 
-  // check for platform collision
-  checkPlatforms();
+    // blue ground
+    fill(0, 100, 225);
+    rect(0, height - 20, width, 20);
 
-  // player movement
-  player.move();
-  player.applyGravity();
-  player.draw();
-
-  // The following 7 lines of code was adapted from https://chatgpt.com/share/676888c8-70f8-8000-a76e-e58a91c788ce Accessed: 2023-12-22
-
-  // scroll platforms at certain height
-  let scrolling = height / 3;
-  if (player.y < scrolling) {
-    let scrollAmount = scrolling - player.y;
-    player.y = scrolling;
-
-    // move platforms downward
+    // move and draw platforms
     for (let platform of platforms) {
-      platform.y += scrollAmount;
+      platform.move();
+      platform.draw();
     }
 
-    // remove platforms that are off-screen
-    platforms = platforms.filter((platform) => platform.y <= height);
+    // check for platform collision
+    checkPlatforms();
 
-    // add new platforms at the top
-    while (platforms.length < 6) {
-      let x = random(width - 150);
+    // player movement
+    player.move();
+    player.applyGravity();
+    player.draw();
 
-      // The following 5 lines of code was adapted from https://chatgpt.com/share/676c0dfa-8418-8000-8206-3e633acd24bf Accessed: 2023-12-25
-      let y;
-      if (platforms.length === 0) {
-        y = -20;
-      } else {
-        y = platforms[platforms.length - 1].y - platformSpacing;
+    // The following 7 lines of code was adapted from https://chatgpt.com/share/676888c8-70f8-8000-a76e-e58a91c788ce Accessed: 2023-12-22
+    // scroll platforms at certain height
+    let scrolling = height / 3;
+    if (player.y < scrolling) {
+      let scrollAmount = scrolling - player.y;
+      player.y = scrolling;
+
+      // move platforms downward
+      for (let platform of platforms) {
+        platform.y += scrollAmount;
       }
 
+      // remove platforms that are off-screen
+      platforms = platforms.filter((platform) => platform.y <= height);
+
+      while (platforms.length < 6) {
+        let x = random(width - 150);
+        // The following line of code was adapted from https://chatgpt.com/share/676c0dfa-8418-8000-8206-3e633acd24bf Accessed: 2023-12-25
+        let y = platforms[platforms.length - 1]?.y - platformSpacing;
+        let type = random(["stationary", "moving", "breaking"]);
+        platforms.push(new Platform(x, y, type));
+      }
+    }
+
+    // check for game over
+    if (player.y > 650) {
+      gameState = "result";
+    }
+  }
+  // result screen
+  else if (gameState === "result") {
+    background(0);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text("Press ENTER to play again", width / 2, height / 2 + 20);
+  }
+}
+
+// start and restart the game
+function keyPressed() {
+  if (keyCode === ENTER && (gameState === "start" || gameState === "result")) {
+    gameState = "playing";
+    player = new Character();
+    platforms = [];
+    for (let i = 0; i < 5; i++) {
+      let x = random(width - 100);
+      let y = height - 150 - i * 150;
       let type = random(["stationary", "moving", "breaking"]);
       platforms.push(new Platform(x, y, type));
     }
